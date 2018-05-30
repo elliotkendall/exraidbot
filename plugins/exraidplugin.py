acad42002fde46d3e4fd0656177aa67b394bc0de
Elliot Kendall <elliotkendall@gmail.com>
2018-05-30 13:27:19 -0700

    Abort if a user is already in a channel

--- plugins/exraidplugin.py
+++ plugins/exraidplugin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from disco.bot import Bot, Plugin, Config
 from disco.types.permissions import PermissionValue, Permissions
+from disco.types.channel import PermissionOverwriteType
 
 import cv2
 import datetime
@@ -57,49 +58,63 @@ class ExRaidPlugin(Plugin):
       if self.dateDiff(date).days < -self.config.old_channel_grace_days:
         channel.delete()
 
+  @staticmethod
+  def userInChannel(user, channel):
+    for snowflake, overwrite in channel.overwrites.iteritems():
+      if overwrite.type == PermissionOverwriteType.MEMBER and overwrite.id == user.id:
+        return True
+    return False
+
   @Plugin.listen('MessageCreate')
   def on_message_create(self, event):
-    if str(event.channel) in self.config.channels_to_watch:
-      for key, value in event.message.attachments.iteritems():
-        # Get the info from the image
+    if not str(event.channel) in self.config.channels_to_watch:
+      return None
+
+    for key, value in event.message.attachments.iteritems():
+      # Get the info from the image
+      try:
+        image = cv2utils.urlToImage(value.url)
+        raidInfo = self.ocr.scanExRaidImage(image, self.top, self.bottom)
+        if self.dateDiff(raidInfo.month + '-' + raidInfo.day + ' ' + raidInfo.begin).days < 0:
+          event.reply(self.config.messages['date_in_past'])
+          continue
+        cname = pokediscord.generateChannelName(raidInfo)
+        catname = pokediscord.generateCategoryName(raidInfo)
+      except Exception:
+        event.reply(self.config.messages['could_not_parse'])
+        continue
+
+      # Create the category if it doesn't exist
+      category = self.getChannelByName(catname, event.guild.channels)
+      if not category:
+        category = event.guild.create_category(catname)
+
+      # Create the channel if it doesn't exist
+      channel = self.getChannelByName(cname, event.guild.channels)
+      if not channel:
         try:
-          image = cv2utils.urlToImage(value.url)
-          raidInfo = self.ocr.scanExRaidImage(image, self.top, self.bottom)
-          if self.dateDiff(raidInfo.month + '-' + raidInfo.day + ' ' + raidInfo.begin).days < 0:
-            event.reply(self.config.messages['date_in_past'])
-            continue          
-          cname = pokediscord.generateChannelName(raidInfo)
-          catname = pokediscord.generateCategoryName(raidInfo)
+          channel = category.create_text_channel(cname)
+          everyone = self.getEveryoneRole(event.guild)
+          channel.create_overwrite(everyone, deny=PermissionValue(Permissions.READ_MESSAGES))
+          for rname in self.config.roles_for_new_channels:
+            role = self.getRoleByName(rname, event.guild)
+            channel.create_overwrite(role, allow=PermissionValue(Permissions.READ_MESSAGES))
         except Exception:
-          event.reply(self.config.messages['could_not_parse'])
+          event.reply(self.config.messages['channel_create_error'])
           continue
 
-        # Create the category if it doesn't exist
-        category = self.getChannelByName(catname, event.guild.channels)
-        if not category:
-          category = event.guild.create_category(catname)
+      # Is the user already in the channel?
+      if self.userInChannel(event.message.author, channel):
+        event.reply(self.config.messages['user_already_in_channel'])
+        continue
 
-        # Create the channel if it doesn't exist
-        channel = self.getChannelByName(cname, event.guild.channels)
-        if not channel:
-          try:
-            channel = category.create_text_channel(cname)
-            everyone = self.getEveryoneRole(event.guild)
-            channel.create_overwrite(everyone, deny=PermissionValue(Permissions.READ_MESSAGES))
-            for rname in self.config.roles_for_new_channels:
-              role = self.getRoleByName(rname, event.guild)
-              channel.create_overwrite(role, allow=PermissionValue(Permissions.READ_MESSAGES))
-          except Exception:
-            event.reply(self.config.messages['channel_create_error'])
-            continue
+      # Add the user to the channel
+      try:
+        channel.create_overwrite(event.message.author, allow=PermissionValue(Permissions.READ_MESSAGES))
+        event.reply('Added ' + event.message.author.username + ' to channel #' + cname)
+      except Exception:
+        event.reply(self.config.messages['channel_add_error'])
+        continue
 
-        # Add the user to the channel
-        try:
-          channel.create_overwrite(event.message.author, allow=PermissionValue(Permissions.READ_MESSAGES))
-          event.reply('Added ' + event.message.author.username + ' to channel #' + cname)
-        except Exception:
-          event.reply(self.config.messages['channel_add_error'])
-          continue
-
-        # Purge old channels
-        self.purgeOldChannels(event.guild.channels)
+      # Purge old channels
+      self.purgeOldChannels(event.guild.channels)
